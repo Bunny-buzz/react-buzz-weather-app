c457cd72cc3d5d56d2771b142a3df3e77652c812
Bunny-buzz <Bunny-buzz@users.noreply.github.com>
2020-09-15 21:17:44 -0600

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather Application</div>;
+  return (
+    <div className="App">
+      Weather Application
+      <footer>
+        Project by Nimi Dada and open sourced on{" "}
+        <a
+          href="https://github.com/Bunny-buzz/react-buzz-weather-app"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
