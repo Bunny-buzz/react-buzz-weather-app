a89665f6d1436d64d380f62b64b4751dd52391d9
Bunny-buzz <Bunny-buzz@users.noreply.github.com>
2020-09-16 20:38:38 -0600

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather Application
-      <footer>
-        Project by Nimi Dada and open sourced on{" "}
-        <a
-          href="https://github.com/Bunny-buzz/react-buzz-weather-app"
-          target="_blank"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        Weather Application
+        <footer>
+          Project by Nimi Dada and open sourced on{" "}
+          <a
+            href="https://github.com/Bunny-buzz/react-buzz-weather-app"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
